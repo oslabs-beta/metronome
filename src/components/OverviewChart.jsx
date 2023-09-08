61179c0e9bd53e20aeb27df5412b5fc861845d3d
Vicky Liu <vlpubi@gmail.com>
2023-09-07 21:34:42 -0500

    hi

--- src/components/OverviewChart.jsx
+++ src/components/OverviewChart.jsx
@@ -2,11 +2,26 @@ import { useState, useEffect } from 'react';
 
 //import css file
 
-const Overview=()=>{
-    
+const Overview=({data})=>{
+    const[version, setVersion]=useState(null);
+    const[totalTime, setTotalTime]=useState(null);
+
+    useEffect(()=>{
+        const overviewData = async()=>{
+            try{
+                setVersion(data.version);
+                setTotalTime(data.timelineData[0].duration)
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+        overviewData();
+    },[])
     return(
         <div className='Overview'>
-            <p>React Version: </p>
+            <p>React Version: {version}</p>
+            <p>Total Render Duration: {totalTime}</p>
         </div>
     )
 }
