de9b8bb0a9c3107b47b80cfbf78a6cf58e2578be
zsugino <124764743+zsugino@users.noreply.github.com>
2023-10-17 10:23:04 +0900

    modified parseTree

--- chrome-extension/src/components/Charts.jsx
+++ chrome-extension/src/components/Charts.jsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import BarChart from './BarChart';
+import BarChart from "./BarChart";
 
-const Charts = () => {
-    return (
-        <div>
-            <BarChart />
-        </div>
-    );
-}
+const Charts = ({ fiberTree }) => {
+  console.log("fiber tree from the charts", fiberTree);
+  return (
+    <div>
+      <BarChart />
+    </div>
+  );
+};
 
 export default Charts;
