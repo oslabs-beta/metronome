6e71c2fd13aac93e106c2a42a4c41a82d2a7e08b
Vicky Liu <vlpubi@gmail.com>
2023-10-21 13:27:39 -0500

    working in App.jsx on curridk

--- chrome-extension/src/components/App.jsx
+++ chrome-extension/src/components/App.jsx
@@ -12,40 +12,47 @@ const App = () => {
   //state for recording status, default to false;
   const [recStat, setRecStat]=useState(false);
   const [recButton, setRecButton]=useState('start profiling');
+  const [index, setIndex]=useState(0);
+  const [curridk, setcurridk]=useState([]);
+  const [idkrun, setidkrun]=useSate(false);
 
   const setStatus=()=>{
-    setRecStat(prevRecStat=>!prevRecStat);
+    setRecStat(prevRecStat=>(!prevRecStat));
 
   }
   //update recButton according to recStat
-  useEffect(()=>{
+  useEffect((idk)=>{
     if(!recStat){
       setRecButton('start profiling');
+
+
     }
-    else{setRecButton('stop profiling');}
+    else{setRecButton('stop profiling');
+
+    if(idk){
+      idk.length===0?setIndex(0):setIndex(idk.length-1);
+    }
+}
   },[recStat])
 
-  //SEND CLEAR EVENT LIST message once recStat changes to true (which means new render list is being generated)
-  useEffect(() => {
-    // Send CLEAR_EVENT_LIST message when recStat changes to true
-    if (recStat && chrome.runtime) {
-      console.log('i am before CLEAR_EVENT_LIST in App.jsx');
-      let port = chrome.runtime.connect({name: "clearEventList"});
-      port.postMessage({action: 'CLEAR_EVENT_LIST'});
-      port.onMessage.addListener((response) => {
-        console.log('CLEAR_EVENT_LIST message sent successfully, response:', response);
-      });
+  //this one updates curridk with index 
+  useEffect(()=>{
+
+    if(idk){
+      setcurridk(idk.slice(index));
     }
-  }, [recStat]);
+
+  },[index,idk])
+
   
   useEffect(() => {
-    //reset eventlist if useEffect triggered by recStat change.
-    // if(recStat){
-    //   if (chrome.runtime) {
-    //     console.log('i am before CLEAR_EVENT_LIST in App.jsx');
-    //     chrome.runtime.sendMessage({ action: 'CLEAR_EVENT_LIST' });
-    //   }
-    // }
+    // reset eventlist if useEffect triggered by recStat change.
+    if(recStat){
+      if (chrome.runtime) {
+        console.log('i am before CLEAR_EVENT_LIST in App.jsx');
+        chrome.runtime.sendMessage({ action: 'CLEAR_EVENT_LIST' });
+      }
+    }
       const msgListener = (request, sender, sendResponse)=>{
         if(recStat){
 
@@ -53,6 +60,7 @@ const App = () => {
           switch(request.action){
             case"EVENT_LIST":
             setIdk([JSON.parse(request.data)]);
+            setidkrun(true);
             break;
         }
         }
@@ -85,7 +93,7 @@ const App = () => {
         <button onClick={()=>setView('chartView')}>Charts</button>
       </nav>
     <div className="container">
-
+      <div className='test'>{JSON.stringify(curridk)}</div>
       {view==='treeView' && <ComponentTree fiberTree={idk} />}
       {view==='chartView' && <Charts/>}
       {/* <img src="metronome.png" />; */}
