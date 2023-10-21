c9964076c72d8a22d3c7c3dd028d2dac62eba2a4
zsugino <124764743+zsugino@users.noreply.github.com>
2023-10-22 01:48:41 +0900

    changes

--- chrome-extension/src/components/ComponentTree.jsx
+++ chrome-extension/src/components/ComponentTree.jsx
@@ -1,68 +1,33 @@
 import React from "react";
 import Tree from "react-d3-tree";
 import "../components/ComponentTree.css";
-import { customStringify, sendData, saveJSON } from "../helperFuncs/helperFuncs";
+import {
+  customStringify,
+  sendData,
+  saveJSON,
+} from "../helperFuncs/helperFuncs";
 // import xhr from '../background/background'
 
 //fiberTree is passed down from App
 function ComponentTree({ fiberTree }) {
-  console.log(fiberTree);
-
-  const getChildren = (tree) => {
-    const children = [];
-    if (!tree) return children;
-    while (tree.sibling) {
-      children.push(tree.sibling);
-      tree = tree.sibling;
-    }
-
-    return children;
-  };
-
-  const parseTree = (tree) => {
-    if (!tree) return;
-    let obj;
-    console.log("parsetree", tree);
-    if (tree === null) return null;
-    else {
-      console.log("result of invokign getChildren", getChildren(tree));
-      if (tree.child) {
-        obj = {
-          name: tree.name,
-          attributes: {
-            actualDuration: tree.actualDuration,
-            selfBaseDuration: tree.selfBaseDuration,
-          },
-          children: [tree.child, ...getChildren(tree.child)].map((elem) =>
-            parseTree(elem)
-          ),
-        };
-      } else {
-        obj = {
-          name: tree.name,
-          attributes: {
-            actualDuration: tree.actualDuration,
-            selfBaseDuration: tree.selfBaseDuration,
-          },
-          children: [...getChildren(tree.child)].map((elem) => parseTree(elem)),
-        };
-      }
-      return obj;
-    }
-  };
-
-  const result= parseTree(fiberTree[0]);
-  //create a new parsedTree with helper function that prevents circular object error.
-  const stringifiedResult = customStringify(result)
-
-  console.log("parsed component tree", parseTree(fiberTree[0]));
+  const stringifiedResult = customStringify(fiberTree);
 
   return (
     <>
-      {result ? (
+      {fiberTree ? (
         <div style={{ width: "60rem", height: "60rem" }}>
-          <button style={{ width: "60rem", height: "3rem" }} onClick={()=> saveJSON(result, 'parseTreeData')}>Click to download file</button>
-          <button style={{ width: "60rem", height: "3rem" }} onClick={()=> sendData(stringifiedResult)}>Click to send data</button>
+          <button
+            style={{ width: "60rem", height: "3rem" }}
+            onClick={() => saveJSON(fiberTree, "parseTreeData")}
+          >
+            Click to download file
+          </button>
+          <button
+            style={{ width: "60rem", height: "3rem" }}
+            onClick={() => sendData(stringifiedResult)}
+          >
+            Click to send data
+          </button>
           <div
             id="treeWrapper"
             style={{
@@ -72,7 +37,7 @@ function ComponentTree({ fiberTree }) {
             }}
           >
             <Tree
-              data={result}
+              data={fiberTree}
               orientation="vertical"
               rootNodeClassName="node__root"
               branchNodeClassName="node__branch"
@@ -87,4 +52,4 @@ function ComponentTree({ fiberTree }) {
   );
 }
 
-export default ComponentTree
+export default ComponentTree;
--- chrome-extension/src/inject/inject.js
+++ chrome-extension/src/inject/inject.js
@@ -4,11 +4,12 @@ const reactDevGlobalHook = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
 // STORE THE PARSED FIBER TREE INFORMATION
 // EACH ELEMENT WILL REPRESENT ONE RERENDER THAT OCCURED
 const eventList = [];
-// FUNCTION: FOR PARSING REACT FIBER TREE
+
 const parseTree = (reactFiberTree) => {
   if (reactFiberTree === null) return null;
   else if (typeof reactFiberTree.elementType === "function") {
-      const elemObj = {
+    // console.log("In the else if block", reactFiberTree.elementType.name);
+    const elemObj = {
       name: reactFiberTree.elementType.name,
       actualDuration: reactFiberTree.actualDuration,
       selfBaseDuration: reactFiberTree.selfBaseDuration,
@@ -17,8 +18,101 @@ const parseTree = (reactFiberTree) => {
     };
     return elemObj;
   } else {
-    return parseTree(reactFiberTree.child) || parseTree(reactFiberTree.sibling);
+    // console.log("In the else block", reactFiberTree.elementType);
+    return {
+      name: "NFC",
+      actualDuration: reactFiberTree.actualDuration,
+      selfBaseDuration: reactFiberTree.selfBaseDuration,
+      child: parseTree(reactFiberTree.child),
+      sibling: parseTree(reactFiberTree.sibling),
+    };
+    // return parseTree(reactFiberTree.child) || parseTree(reactFiberTree.sibling);
   }
+};
+
+const getChildren = (tree) => {
+  const children = [];
+  if (!tree) return children;
+  while (tree.sibling) {
+    children.push(tree.sibling);
+    tree = tree.sibling;
+  }
+
+  return children;
+};
+
+const parseTreeInTreeStructure = (tree) => {
+  if (!tree) return;
+  let obj;
+  console.log("parsetree", tree);
+  if (tree === null) return null;
+  else {
+    console.log("result of invokign getChildren", getChildren(tree));
+    if (tree.child) {
+      obj = {
+        name: tree.name,
+        actualDuration: tree.actualDuration,
+        selfBaseDuration: tree.selfBaseDuration,
+        children: [tree.child, ...getChildren(tree.child)].map((elem) =>
+          parseTreeInTreeStructure(elem)
+        ),
+      };
+    } else {
+      obj = {
+        name: tree.name,
+        actualDuration: tree.actualDuration,
+        selfBaseDuration: tree.selfBaseDuration,
+        children: [...getChildren(tree.child)].map((elem) =>
+          parseTreeInTreeStructure(elem)
+        ),
+      };
+    }
+    return obj;
+  }
+};
+
+const removeNFCsFromChildArray = (tree) => {
+  if (tree.children === null) return null;
+  let parsedChildArray = [];
+  for (let i = 0; i < tree.children.length; i++) {
+    const el = tree.children[i];
+    if (el.name === "NFC") {
+      parsedChildArray = parsedChildArray.concat(
+        removeNFCsFromChildArray({
+          name: tree.name,
+          actualDuration: tree.actualDuration,
+          selfBaseDuration: tree.selfBaseDuration,
+          children: el.children,
+        })
+      );
+    } else parsedChildArray.push(el);
+  }
+
+  console.log("Parsed Child Array", parsedChildArray);
+  return parsedChildArray;
+};
+
+const removeAllNFCs = (tree) => {
+  //ASSUMING THAT THE ROOT NODE OF TREE IS NOT A NFC
+  const immediateChildren = removeNFCsFromChildArray(tree);
+  const actualChildren = immediateChildren.map((child) => {
+    return {
+      name: child.name,
+      actualDuration: child.actualDuration,
+      selfBaseDuration: child.selfBaseDuration,
+      children: removeAllNFCs(child),
+    };
+  });
+  return actualChildren;
+};
+
+const final = (tree) => {
+  return {
+    name: tree.name,
+    actualDuration: tree.actualDuration,
+    selfBaseDuration: tree.selfBaseDuration,
+    children: removeAllNFCs(tree),
+  };
 };
 
 // FUNCTION: CREATING CUSTOMIZED ONCOMMITFIBER ROOT FUNCTION
@@ -30,15 +124,13 @@ const customOnCommitFiberRoot = (onCommitFiberRoot) => {
       fiberRoot
     );
     console.log("this is the unparsed tree", fiberRoot.current);
-    eventList.push(parseTree(fiberRoot.current));
-    const eventListStr = JSON.stringify(eventList[eventList.length - 1]);
-
-    console.log("current fiber tree", eventList[eventList.length - 1]);
+    eventList.push(
+      final(parseTreeInTreeStructure(parseTree(fiberRoot.current)))
+    );
+    const eventListStr = JSON.stringify(eventList);
 
     window.postMessage({ type: "EVENT_LIST", eventListStr });
 
-    console.log("INJECT.JS: eventList", eventList);
-    console.log();
     return onCommitFiberRoot(...args);
   };
 };
@@ -49,4 +141,3 @@ reactDevGlobalHook.onCommitFiberRoot = customOnCommitFiberRoot(
 );
 
 // REACT DEV TOOL GLOBAL HOOK
-
