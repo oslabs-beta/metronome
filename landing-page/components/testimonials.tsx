fc1ba2d92be3dade907bf3f1676dbacf92364c0d
j9peters <29951738+j9peters@users.noreply.github.com>
2023-11-07 22:47:53 -0800

    centered engineer bio content

--- landing-page/components/testimonials.tsx
+++ landing-page/components/testimonials.tsx
@@ -22,66 +22,66 @@ export default function Testimonials() {
           <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
 
             {/* 1st testimonial */}
-            <div className="flex flex-col h-full p-6 bg-gray-800 " data-aos="fade-up">
+            <div className="flex flex-col h-full p-6 bg-gray-800 text-center" data-aos="fade-up">
               <div>
-                <div className="relative inline-flex flex-col mb-4">
+                <div className="relative inline-flex flex-col align-middle">
                   <Image className="rounded-full" src={Bruce} width={100} height={100} alt="Testimonial 01" />
                 </div>
               </div>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <p className="text-gray-200 not-italic">Bruce Onuigbo -</p><p className="text-purple-600">Fullstack Software Engineer</p>
+                <p className="text-gray-200 not-italic">Bruce Onuigbo</p><p className="text-purple-600">Full-Stack Software Engineer</p>
               </div>
             </div>
 
 
             {/* 2nd testimonial */}
-            <div className="flex flex-col h-full p-6 bg-gray-800 " data-aos="fade-up">
+            <div className="flex flex-col h-full p-6 bg-gray-800 text-center" data-aos="fade-up">
               <div>
-                <div className="relative inline-flex flex-col mb-4 ">
+                <div className="relative inline-flex flex-col align-middle">
                   <Image className="rounded-full" src={Jeanine} width={100} height={100} alt="Testimonial 01" />
                 </div>
               </div>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <p className="text-gray-200 not-italic">Jeanine Peters -</p><p className="text-purple-600">Fullstack Software Engineer</p>
+                <p className="text-gray-200 not-italic">Jeanine Peters</p><p className="text-purple-600">Full-Stack Software Engineer</p>
               </div>
             </div>
 
 
             {/* 3rd testimonial */}
-            <div className="flex flex-col h-full p-6 bg-gray-800 " data-aos="fade-up">
+            <div className="flex flex-col h-full p-6 bg-gray-800 text-center" data-aos="fade-up">
               <div>
-                <div className="relative inline-flex flex-col mb-4 ">
+                <div className="relative inline-flex flex-col align-middle">
                   <Image className="rounded-full" src={Lisandro} width={100} height={100} alt="Testimonial 01" />
                 </div>
               </div>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <p className="text-gray-200 not-italic">Lisandro Olivares -</p><p className="text-purple-600">Fullstack Software Engineer</p>
+                <p className="text-gray-200 not-italic">Lisandro Olivares</p><p className="text-purple-600">Full-Stack Software Engineer</p>
               </div>
             </div>
 
 
             {/* 4th testimonial */}
-            <div className="flex flex-col h-full p-6 bg-gray-800 " data-aos="fade-up">
+            <div className="flex flex-col h-full p-6 bg-gray-800 text-center" data-aos="fade-up">
               <div>
-                <div className="relative inline-flex flex-col mb-4 ">
+                <div className="relative inline-flex flex-col align-middle">
                   <Image className="rounded-full" src={Vicky} width={100} height={100} alt="Testimonial 01" />
                 </div>
               </div>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <p className="text-gray-200 not-italic">Vicky Liu -</p><p className="text-purple-600">Fullstack Software Engineer</p>
+                <p className="text-gray-200 not-italic">Vicky Liu</p><p className="text-purple-600">Full-Stack Software Engineer</p>
               </div>
             </div>
 
 
             {/* 5th testimonial */}
-            <div className="flex flex-col h-full p-6 bg-gray-800 " data-aos="fade-up">
+            <div className="flex flex-col h-full p-6 bg-gray-800 text-center" data-aos="fade-up">
               <div>
-                <div className="relative inline-flex flex-col mb-4 ">
+                <div className="relative inline-flex flex-col align-middle">
                   <Image className="rounded-full" src={Zai} width={100} height={100} alt="Testimonial 01" />
                 </div>
               </div>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <p className="text-gray-200 not-italic">Zai Sugino -</p><p className="text-purple-600">Fullstack Software Engineer</p>
+                <p className="text-gray-200 not-italic">Zai Sugino</p><p className="text-purple-600">Full-Stack Software Engineer</p>
               </div>
             </div>
 
